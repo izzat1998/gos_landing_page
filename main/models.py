f73f4ed5760ab03d02bb61fb53b49ccb96abe1cc
izzat1998 <izzattilla706@gmail.com>
2025-05-14 13:59:59 +0500

    feat: add Contact model for storing user inquiries and feedback

--- main/models.py
+++ main/models.py
@@ -1,25 +1,40 @@
+from django.core.exceptions import ProtectedError
 from django.db import models
 
 # Create your models here.
+
+
+class LocationQuerySet(models.QuerySet):
+    def delete(self, *args, **kwargs):
+        raise ProtectedError("Удаление локаций запрещено.", self)
+
 
 class Location(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
+    objects = LocationQuerySet.as_manager()
+
+    def delete(self, *args, **kwargs):
+        raise ProtectedError("Удаление локаций запрещено.", self)
+
     def __str__(self):
         return self.name
-    
+
     class Meta:
-        verbose_name = 'Локация'
-        verbose_name_plural = 'Локации'
+        verbose_name = "Локация"
+        verbose_name_plural = "Локации"
+
 
 class QRCodeScan(models.Model):
-    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='scans')
+    location = models.ForeignKey(
+        Location, on_delete=models.PROTECT, related_name="scans"
+    )
     timestamp = models.DateTimeField(auto_now_add=True)
     ip_address = models.GenericIPAddressField(null=True, blank=True)
     user_agent = models.TextField(blank=True)
-    
+
     class Meta:
-        verbose_name = 'Скан QR-кода'
-        verbose_name_plural = 'Сканы QR-кодов'
+        verbose_name = "Скан QR-кода"
+        verbose_name_plural = "Сканы QR-кодов"
