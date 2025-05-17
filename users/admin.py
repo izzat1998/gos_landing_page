79f5eed067c001692631727f9e34b1512de5de4a
izzat1998 <izzattilla706@gmail.com>
2025-05-17 12:16:57 +0500

    feat: enhance CustomUser admin interface with custom fields and improved display options

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'phone_number', 'telegram_id', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('username', 'first_name', 'last_name', 'email', 'phone_number', 'telegram_id')
+    
+    # Add the custom fields to the fieldsets
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Contact info'), {'fields': ('phone_number', 'telegram_id')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    # Add the custom fields to the add_fieldsets
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'email', 'phone_number', 'telegram_id'),
+        }),
+    )
