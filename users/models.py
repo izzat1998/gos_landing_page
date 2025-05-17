1351242845a2f81e578d4e6558931589c4b237f6
izzat1998 <izzattilla706@gmail.com>
2025-05-17 11:48:50 +0500

    feat: add telegram_id field and increase phone_number length in CustomUser model

--- users/models.py
+++ users/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    phone_number = models.CharField(max_length=15)
-    # add additional fields in here
+    phone_number = models.CharField(max_length=20)
+    telegram_id = models.CharField(max_length=20)
 
     def __str__(self):
         return self.username
